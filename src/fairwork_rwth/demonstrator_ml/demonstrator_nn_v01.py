7f692a8e62e1cfa32ef3158480804a46e0b04f47
Alexander Nasuta <alexander.nasuta@rwth-aachen.de>
2023-07-25 11:11:54 +0200

    demonstrator first version - api endpoint - trained model

--- src/fairwork_rwth/demonstrator_ml/demonstrator_nn_v01.py
+++ src/fairwork_rwth/demonstrator_ml/demonstrator_nn_v01.py
@@ -25,63 +25,48 @@ class DemonstratorNeuralNet(nn.Module):
 
 
 class CustomDataset(Dataset):
-    def __init__(self, data_directory: str | pl.Path, batch_size):
+    def __init__(self, data_directory: str | pl.Path):
         self.data_directory = pl.Path(data_directory)
         self.filenames = list(self.data_directory.glob("*.csv"))
-        self.batch_size = batch_size
 
         if not len(self.filenames):
             raise ValueError(f"no .csv files found in '{self.data_directory}'")
 
-        sample_file = pd.read_csv(self.filenames[0]).to_numpy()
-        self.file_shape = sample_file.shape
+        sample_file = pd.read_csv(self.filenames[0])
+        # drop index column
+        sample_file = sample_file.drop(sample_file.columns[0], axis=1)
+        sample_y = sample_file.pop("final allocation")
 
-        self.n_y_params = 1
-        self.n_x_params = self.file_shape[1] - 1
+        self.n_y_params, *_ = np.ravel(sample_y.to_numpy()).shape
+        self.n_x_params, *_ = np.ravel(sample_file.to_numpy()).shape
 
     def __len__(self):
-        return int(np.ceil(
-            len(self.filenames) /
-            float(self.batch_size)
-        ))  # Number of chunks.
+        return len(self.filenames)
 
     def __getitem__(self, idx):  # idx means index of the chunk.
         # In this method, we do all the preprocessing.
         # First read data from files in a chunk. Preprocess it. Extract labels. Then return data and labels.
-        batch_x = self.filenames[
-                  idx * self.batch_size
-                  :
-                  (idx + 1) * self.batch_size
-                  ]  # This extracts one batch of file names from the list `filenames`.
+        file = self.filenames[idx]
 
-        x_data = []
-        y_data = []
-
-        for file in batch_x:
-            temp = pd.read_csv(open(file, 'r'))  # Change this line to read any other type of file
-            y = temp.pop("final allocation")
-            x = temp.to_numpy()
-            # flatten x
-            x = x.ravel()
-            x_data.append(x)
-            y_data.append(y.to_numpy())
+        df = pd.read_csv(open(file, 'r'))
+        # drop index column
+        df = df.drop(df.columns[0], axis=1)
+        y_data = np.ravel(df.pop("final allocation").to_numpy())
+        x_data = np.ravel(df.to_numpy())
 
         # The following condition is actually needed in Pytorch. Otherwise, for our particular example, the iterator will be an infinite loop.
         # Readers can verify this by removing this condition.
         if idx == self.__len__():
             raise IndexError
 
-        # convert list of numpy arrays to one numpy array
-        x_data = np.array(x_data)
-        y_data = np.array(y_data)
         return x_data, y_data
 
 
-def train_demonstrator_model(n_epochs:int) -> None:
+def train_demonstrator_model(n_epochs: int) -> None:
     # list all .csv files in the training-data directory
     data_dir_path = pl.Path("./resources").joinpath("training-data").joinpath("demonstrator-v01")
 
-    dataset = CustomDataset(data_directory=data_dir_path, batch_size=5)
+    dataset = CustomDataset(data_directory=data_dir_path)
 
     model = DemonstratorNeuralNet(
         input_dim=dataset.n_x_params,
@@ -101,17 +86,79 @@ def train_demonstrator_model(n_epochs:int) -> None:
 
     # define pytorch training loop
     for epoch in range(n_epochs):
-        for batch_idx, (data, target) in enumerate(data_loader):
+        for batch_idx, (x, y) in enumerate(data_loader):
+            # x = x.float()
+            # y = y.float()
             optimizer.zero_grad()
-            print(data.shape)
-            output = model(data)
-            loss = loss_fn(output, target)
+            y_pred = model(x.to(torch.float32)).to(torch.float32)
+            loss = loss_fn(y_pred, y.to(torch.float32))
             loss.backward()
             optimizer.step()
-            if batch_idx % 10 == 0:
-                log.info(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(data_loader.dataset)} "
-                         f"({100. * batch_idx / len(data_loader):.0f}%)]\tLoss: {loss.item():.6f}")
+            log.info(f"epoch: {epoch}, batch_idx: {batch_idx}, loss: {loss.item()}")
+
+    # save model
+    torch.save(model.state_dict(), "./resources/trained-models/demonstrator-v01/model1.pt")
+
+
+def get_model() -> DemonstratorNeuralNet:
+    if not pl.Path("./resources/trained-models/demonstrator-v01/model1.pt").exists():
+        raise FileNotFoundError("could not find model1.pt. run 'train_demonstrator_model' first.")
+    # load model
+
+    # list all .csv files in the training-data directory
+    data_dir_path = pl.Path("./resources").joinpath("training-data").joinpath("demonstrator-v01")
+    dataset = CustomDataset(data_directory=data_dir_path)
+
+    model = DemonstratorNeuralNet(
+        input_dim=dataset.n_x_params,
+        hidden_dim=10,
+        output_dim=dataset.n_y_params
+    )
+    model.load_state_dict(torch.load("./resources/trained-models/demonstrator-v01/model1.pt"))
+
+    return model
+
+
+def processed_prediction(datapoint: np.ndarray, n_workers) -> np.ndarray:
+    model = get_model()
+    # predict
+    y_pred = model(torch.from_numpy(datapoint).to(torch.float32))
+    # torch to numpy
+    y_pred = y_pred.detach().numpy()
+    # set n_workers largest values to 1, rest to 0
+    y_pred = np.array([1 if i in np.argpartition(y_pred, -n_workers)[-n_workers:] else 0 for i in range(len(y_pred))])
+    return y_pred
+
+
+def prediction_example() -> None:
+    # get random csv file from training-data directory
+    files = pl.Path("./resources").joinpath("training-data").joinpath("demonstrator-v01").glob("*.csv")
+    file_name = np.random.choice(list(files))
+
+    # read csv file
+    df = pd.read_csv(file_name)
+    # drop index column
+    df = df.drop(df.columns[0], axis=1)
+    y_data = df.pop("final allocation")
+    log.info(f"input_data cols: {df.columns}")
+    log.info(f"input_data:")
+    log.info(df.head())
+
+    x_data = df.to_numpy()
+    log.info(f"not flattered input_data as numpy array (shape: {x_data.shape}): \n{x_data}")
+
+    # y_data = np.ravel(y_data.to_numpy())
+    x_data = np.ravel(x_data)
+
+    log.info(f"input_data as numpy array (shape: {x_data.shape}): \n{x_data}")
+
+    # predict
+    y_pred = processed_prediction(x_data, n_workers=4)
+    log.info(f"y_pred: {y_pred}")
 
 
 if __name__ == '__main__':
     train_demonstrator_model(n_epochs=5)
+    model = get_model()
+    log.info(f"trained model: {model}, file: ./resources/trained-models/demonstrator-v01/model1.pt")
+    prediction_example()
---
+++ src/fairwork_rwth/rest/faskx_demo_endpoint.py
@@ -0,0 +1,73 @@
+import random
+
+import pandas as pd
+import numpy as np
+
+from fairwork_rwth.demonstrator_ml.demonstrator_nn_v01 import processed_prediction
+from fairwork_rwth.utils.logger import log
+
+from flask import Flask
+from flask_restx import Api, Resource, fields
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version='0.1', title='AI models API',
+          description='AI Enrichment FAIRWork Demonstrator.',
+          )
+
+ns = api.namespace('demonstrator-nn', description='Endpoint calls')
+
+demonstrator_output_model = api.model('DemonstratorInput', {
+    'allocation': fields.List(required=True, description='worker allocation.', cls_or_instance=fields.Float)
+})
+
+
+input_fields = ["ID", "Woker avaibale", "Medical condtion", "Efficiency on the line", "Difficulty of the geometry",
+                "production priority", "due date in days", "UTE allocation", "worker preference"]
+demonstrator_input_model = api.model('DemonstratorInput', {
+    **{
+        field: fields.List(required=True, description=f'{field} colum', cls_or_instance=fields.Float)
+        for field in input_fields
+    },
+    'num_workers': fields.Integer(required=True, description='number of workers to allocate.')
+})
+
+
+@ns.route('/')
+class DemonstratorNN(Resource):
+
+    @ns.doc('allocate_workers')
+    @ns.expect(demonstrator_input_model)
+    @ns.marshal_list_with(demonstrator_output_model)
+    def post(self):
+        """perform a prediction on the given input data. `num_workers` is the number of workers to allocate."""
+        log.info("received request to perform a prediction on the given input data.")
+        log.info(f"payload: {api.payload}")
+
+        data = dict(api.payload)
+        n_workers = data.pop("num_workers")
+
+        df = pd.DataFrame(data)
+
+        log.info(f"input_data (shape: {df.shape}): \n{df.head()}")
+
+        x_data = np.ravel(df.to_numpy())
+
+        if x_data.shape != (1431,):
+            raise ValueError(f"expected shape (1431,), but got {x_data.shape}."
+                             f"make sure to provide all list input fields have length '159'.")
+
+        prediction = processed_prediction(x_data, n_workers=n_workers)
+
+        return {"allocation": prediction}
+
+
+def main() -> None:
+    log.info("starting flask app...")
+    app.run()
+
+
+if __name__ == '__main__':
+    # print([random.random() for _ in range(159)])
+    main()
